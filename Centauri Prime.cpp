01386d9d4eb221ccb14c8ff7a2cdd064cfba7f75
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2022-02-16 17:56:26 +0200

    Create Centauri Prime.cpp

---
+++ Centauri Prime.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string GetRuler(const string& kingdom) {
+  // TODO: implement this method to determine the ruler name, given the kingdom.
+    string s = kingdom;
+    string ch = s.substr(s.size() - 1);
+    string ruler = "";
+    if (ch == "y") {
+        return "nobody";
+    }
+    if (ch == "a" || ch=="e" || ch=="i" || ch=="o" ||ch== "u") {
+        return "Alice";
+    }
+    else {
+        return "Bob";
+    }
+}
+int main() {
+  int testcases;
+  cin >> testcases;
+  string kingdom;
+
+  for (int t = 1; t <= testcases; ++t) {
+    cin >> kingdom;
+    cout << "Case #" << t << ": " << kingdom << " is ruled by "
+         << GetRuler(kingdom) << ".\n";
+  }
+  return 0;
+}
